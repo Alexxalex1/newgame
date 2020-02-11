3cc89e02d7beb380fd90edec131220ea6fef5608
Alexxalex1 <alxsar98@gmail.com>
2020-02-12 00:29:07 +0300

    пофиксил ошибку в консоли

--- vue.js
+++ vue.js
@@ -1,38 +1,36 @@
-
 var VueJs = new Vue({
-      el: '#app',
-      data: {
+    el: '#app',
+    data: {
         name: 'Vue.js',
         counter: 0
-      },
-      methods: {
+    },
+    methods: {
         again () {
-          if (document.querySelector('.again')) {
-            return window.location.reload()
-          }
+            if (document.querySelector('.again')) {
+                return window.location.reload()
+                // this.scene.start('preload')
+            }
         }
-      }
-    })
-    var isActive = false
-    var isActiveRight = false
-    var isActiveUp = false
-    document.querySelector('.left').addEventListener('pointerdown', e => {
-      isActive = true
-    })
-    document.querySelector('.left').addEventListener('pointerup', e => {
-      isActive = false
-    })
-    document.querySelector('.right').addEventListener('pointerdown', e => {
-      isActiveRight = true
-    })
-    document.querySelector('.right').addEventListener('pointerup', e => {
-      isActiveRight = false
-    })
-    document.querySelector('.up').addEventListener('pointerdown', e => {
-      isActiveUp = true
-    })
-    document.querySelector('.up').addEventListener('pointerup', e => {
-      isActiveUp = false
-    })
-
-
+    }
+})
+var isActive = false
+var isActiveRight = false
+var isActiveUp = false
+/*document.querySelector('.left').addEventListener('pointerdown', e => {
+    isActive = true
+})
+document.querySelector('.left').addEventListener('pointerup', e => {
+    isActive = false
+})
+document.querySelector('.right').addEventListener('pointerdown', e => {
+    isActiveRight = true
+})
+document.querySelector('.right').addEventListener('pointerup', e => {
+    isActiveRight = false
+})
+document.querySelector('.up').addEventListener('pointerdown', e => {
+    isActiveUp = true
+})
+document.querySelector('.up').addEventListener('pointerup', e => {
+    isActiveUp = false
+})*/
