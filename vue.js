e9d5482b1665e88a89cbd9a306a407968591373a
Alexxalex1 <alxsar98@gmail.com>
2020-02-11 22:34:32 +0300

    Initial commit

---
+++ vue.js
@@ -0,0 +1,38 @@
+
+var VueJs = new Vue({
+      el: '#app',
+      data: {
+        name: 'Vue.js',
+        counter: 0
+      },
+      methods: {
+        again () {
+          if (document.querySelector('.again')) {
+            return window.location.reload()
+          }
+        }
+      }
+    })
+    var isActive = false
+    var isActiveRight = false
+    var isActiveUp = false
+    document.querySelector('.left').addEventListener('pointerdown', e => {
+      isActive = true
+    })
+    document.querySelector('.left').addEventListener('pointerup', e => {
+      isActive = false
+    })
+    document.querySelector('.right').addEventListener('pointerdown', e => {
+      isActiveRight = true
+    })
+    document.querySelector('.right').addEventListener('pointerup', e => {
+      isActiveRight = false
+    })
+    document.querySelector('.up').addEventListener('pointerdown', e => {
+      isActiveUp = true
+    })
+    document.querySelector('.up').addEventListener('pointerup', e => {
+      isActiveUp = false
+    })
+
+
